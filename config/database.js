4fcf607f8b604f713769bcaa1d86a66a5b1078f6
Young Rich <tajiriramadhan@gmail.com>
2024-09-01 14:01:24 +0300

    debuging the database configuration file

--- config/database.js
+++ config/database.js
@@ -4,14 +4,14 @@ import url from 'url';
 // Parse the JAWSDB_URL environment variable
 const dbUrl = new url.URL(process.env.JAWSDB_URL);
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: dbUrl.hostname,
   user: dbUrl.username,
   password: dbUrl.password,
   database: dbUrl.pathname.substring(1), // Temporarily connect to the existing database
 });
 
-connection.connect(err => {
+db.connect(err => {
   if (err) {
     console.error('Initial database connection failed:', err.stack);
     return;
@@ -19,7 +19,7 @@ connection.connect(err => {
   console.log('Connected to the existing database.');
 
   // Check if the db_restaurant database exists and create it if not
-  connection.query(`CREATE DATABASE IF NOT EXISTS db_restaurant`, (err, results) => {
+  db.query(`CREATE DATABASE IF NOT EXISTS db_restaurant`, (err, results) => {
     if (err) {
       console.error('Failed to create db_restaurant database:', err.stack);
       return;
