9e4754592d66d8210b8ea817416dc32a9eeea55e
Young Rich <tajiriramadhan@gmail.com>
2024-09-01 13:44:31 +0300

    changing the db connection parameters

--- config/database.js
+++ config/database.js
@@ -2,17 +2,17 @@ import mysql from "mysql2";
 
 // create the connection to database
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "db_restaurant"
+
+const connection = mysql.createConnection({
+  host: process.env.JAWSDB_URL.split('@')[1].split('/')[0],
+  user: process.env.JAWSDB_URL.split('//')[1].split(':')[0],
+  password: process.env.JAWSDB_URL.split(':')[2].split('@')[0],
+  database: 'db_restaurant',
 });
 
-
-db.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected to db_restaurant database.');
 });
 
 export default db;
