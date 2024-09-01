be1c99698f846de6751394aaec0ac44702d4fbe7
Young Rich <tajiriramadhan@gmail.com>
2024-09-01 13:25:16 +0300

    changing the port configurations

--- index.js
+++ index.js
@@ -81,59 +81,8 @@ app.use((err, req, res, next) => {
     console.log(err)
 });
 
-// Normalize a port into a number, string, or false.
-function normalizePort(val) {
-    const port = parseInt(val, 10);
+const PORT = process.env.PORT || 3000;
 
-    if (isNaN(port)) {
-        return val; // named pipe
-    }
-
-    if (port >= 0) {
-        return port; // port number
-    }
-
-    return false;
-}
-
-// Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || '8081');
-app.set('port', port);
-
-// Create HTTP server.
-const server = http.createServer(app);
-
-// Listen on provided port, on all network interfaces.
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-// Event listener for HTTP server "error" event.
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-// Event listener for HTTP server "listening" event.
-function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Listening on ' + bind);
-}
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
