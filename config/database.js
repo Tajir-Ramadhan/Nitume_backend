5bc11dba0105f266015433877fa7ae99a7cdc616
Young Rich <tajiriramadhan@gmail.com>
2024-09-01 13:47:05 +0300

    changing connection to db

--- config/database.js
+++ config/database.js
@@ -3,14 +3,14 @@ import mysql from "mysql2";
 // create the connection to database
 
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.JAWSDB_URL.split('@')[1].split('/')[0],
   user: process.env.JAWSDB_URL.split('//')[1].split(':')[0],
   password: process.env.JAWSDB_URL.split(':')[2].split('@')[0],
   database: 'db_restaurant',
 });
 
-connection.connect(err => {
+db.connect(err => {
   if (err) throw err;
   console.log('Connected to db_restaurant database.');
 });
